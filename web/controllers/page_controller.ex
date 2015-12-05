f0adedcc34caecdc4deed7d1af0b909fdc7c9679
Tim Cocu <timcocu@gmail.com>
2015-12-05 16:11:41 +0100

    Update index page with panels for all products

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,10 @@
 defmodule Micro.PageController do
   use Micro.Web, :controller
 
+  alias Micro.Product
+
   def index(conn, _params) do
-    render conn, "index.html"
+    products = Repo.all(Product)
+    render conn, "index.html", products: products
   end
 end
