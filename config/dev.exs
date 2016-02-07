6d0b8be710b38a5a897c8463bfe3224ef64f635c
Tim Cocu <timcocu@gmail.com>
2016-02-07 14:20:08 +0100

    Update .gitlab-ci.yml

---
+++ config/dev.exs
@@ -0,0 +1,17 @@
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :micro, Micro.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Configure your database
+config :micro, Micro.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "micro_dev",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
