04000ac2f2aee22593d28b8343e41aa0145bef31
Tim Cocu <timcocu@gmail.com>
2016-02-07 17:44:56 +0100

    Update .gitlab-ci.yml

--- test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Micro.PageControllerTest do
-  use Micro.ConnCase
-
-  test "GET /" do
-    conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
