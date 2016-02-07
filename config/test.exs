73ab4930d4bbde291ee260bf9fae9649bf2602fd
Tim Cocu <timcocu@gmail.com>
2016-02-07 18:02:55 +0100

    Update .gitlab-ci.yml

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :micro, Micro.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "micro_test",
   database: "micro_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
